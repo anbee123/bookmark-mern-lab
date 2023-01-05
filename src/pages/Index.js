d1e65b7fe3e7c1ad8fef58273af4bacef550291c
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-01-04 19:16:59 -0600

    initial react setup

---
+++ src/pages/Index.js
@@ -0,0 +1,5 @@
+function Index(props) {
+    return <h3>List of bookmarks</h3>;
+  }
+  
+  export default Index;
---
+++ src/pages/Show.js
@@ -0,0 +1,5 @@
+function Show(props) {
+    return <h1>Update bookmark</h1>;
+  }
+  
+  export default Show;
